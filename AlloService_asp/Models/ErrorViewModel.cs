42cfec4fa3ff96b73f9b3fa1e7741cd1074b41ae
mohammedjellab <mohammedjellab@gmail.com>
2018-08-30 08:33:12 +0100

    First Step

---
+++ AlloService_asp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlloService_asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
